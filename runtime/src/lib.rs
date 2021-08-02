9ebc2317a96dd27f9e7833275d1549cbce26a751
yan0327 <yan0327@908141467>
2021-08-02 18:50:12 +0800

    tutorials/solutions/build-a-dapp-v3+1

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -270,6 +270,10 @@ impl pallet_template::Config for Runtime {
 	type Event = Event;
 }
 
+impl pallet_poe::Config for Runtime {
+	type Event = Event;
+}
+
 // Create the runtime by composing the FRAME pallets that were previously configured.
 construct_runtime!(
 	pub enum Runtime where
@@ -287,6 +291,7 @@ construct_runtime!(
 		Sudo: pallet_sudo::{Module, Call, Config<T>, Storage, Event<T>},
 		// Include the custom logic from the pallet-template in the runtime.
 		TemplateModule: pallet_template::{Module, Call, Storage, Event<T>},
+		PoeModule: pallet_poe::{Module, Call, Storage, Event<T>},
 	}
 );
 
